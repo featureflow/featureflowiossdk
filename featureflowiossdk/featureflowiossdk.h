3b6dfe68f6a45145707894da795dba53b0f785f1
mmattini <maxmattini@gmail.com>
2017-06-26 12:29:45 +1000

    Initial Commit

---
+++ featureflowiossdk/featureflowiossdk.h
@@ -0,0 +1,19 @@
+//
+//  featureflowiossdk.h
+//  featureflowiossdk
+//
+//  Created by Max Mattini on 26/06/2017.
+//  Copyright © 2017 Max Mattini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for featureflowiossdk.
+FOUNDATION_EXPORT double featureflowiossdkVersionNumber;
+
+//! Project version string for featureflowiossdk.
+FOUNDATION_EXPORT const unsigned char featureflowiossdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <featureflowiossdk/PublicHeader.h>
+
+
